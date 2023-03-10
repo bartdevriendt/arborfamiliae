369de7dab8e6b703eed32cdc61eb6be91cc5637b
Bart Devriendt <bart.devriendt@outlook.com>
2023-03-10 17:25:01 +0100

    Add tests

---
+++ ArborFamiliae.Services.IntegrationTests/Genealogy/GenderServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using ArborFamiliae.Services.Genealogy;
+
+namespace ArborFamiliae.Services.IntegrationTests.Genealogy;
+
+public class GenderServQiceTests : TestBase
+{
+    [Test]
+    public async Task Test_LoadAllGenders()
+    {
+        // arrange
+        var genderService = GetService<GenderService>();
+
+        // act
+        var genders = await genderService.GetGenders();
+
+        // assert
+        await Verify(genders).ToTask();
+    }
+}
