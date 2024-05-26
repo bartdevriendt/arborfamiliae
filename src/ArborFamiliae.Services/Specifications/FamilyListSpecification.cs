6e30336bd23340afc1072528eb28f57d7301b221
Bart Devriendt <bart.devriendt@gmail.com>
2024-05-26 18:34:01 +0200

    Add family list

--- src/ArborFamiliae.Services/Specifications/FamilyListSpecification.cs
+++ src/ArborFamiliae.Services/Specifications/FamilyListSpecification.cs
@@ -28,5 +28,7 @@ public class FamilyListSpecification : Specification<Family>
                 .ThenInclude(c => c.Names)
                 .ThenInclude(e => e.Surnames);
         }
+
+        Query.AsSplitQuery();
     }
 }
