88f8cb3b78d3b122842fbb988278ff2d9eec6efb
Bart Devriendt <bart.devriendt@gmail.com>
2024-06-01 19:43:30 +0200

    Begin new family tree popup

--- src/ArborFamiliae.Blazor.WinForms/Form1.Designer.cs
+++ src/ArborFamiliae.Blazor.WinForms/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.ClientSize = new System.Drawing.Size(1024, 768);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Arbor Familiae";
             this.ResumeLayout(false);
 
         }
---
+++ src/ArborFamiliae.Domain/Database/DatabaseType.cs
@@ -0,0 +1,7 @@
+﻿namespace ArborFamiliae.Domain.Database;
+
+public class DatabaseType
+{
+    public string Key { get; set; }
+    public string Description { get; set; }
+}
